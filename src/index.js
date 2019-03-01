26e8bbcd23a91dfd60663e8ae5658232e7d50fd2
GreRin <rigor1988@gmail.com>
2019-03-01 13:30:55 +0300

    Did wurmup task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
-};
+	const far = temperature * 9 / 5 +32
+	if (far > -94 && far < 140) {
+		if (far <= -4) {
+			console.log('warm cold')
+		} else if (far <= 32) {
+			console.log('warm cool')
+		} else if (far <= 59) {
+			console.log('warm medium')
+		} else if (far > 59) {
+			console.log('warm hot')
+		}
+		return far
+	} else {
+		console.log('Введено неверное значение')
+	}
+}
